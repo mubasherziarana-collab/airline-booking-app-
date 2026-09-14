1a4d1ec8c94f53caedfa977483735ebcdb61510d
zia rana <mubasherziarana@gmail.com>
2026-09-13 22:01:48 -0600

    fixed expedia widget blank screen with iframe isolation

--- src/components/ExpediaWidget.jsx
+++ src/components/ExpediaWidget.jsx
@@ -1,34 +1,14 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 
 const ExpediaWidget = () => {
-  const scriptContainerRef = useRef(null);
-
-  useEffect(() => {
-    // Only inject the script if it hasn't been injected yet
-    if (scriptContainerRef.current && scriptContainerRef.current.children.length === 0) {
-      const script = document.createElement('script');
-      script.className = "eg-widgets-script";
-      script.src = "https://creator.expediagroup.com/products/widgets/assets/eg-widgets.js";
-      script.async = true;
-      scriptContainerRef.current.appendChild(script);
-    }
-  }, []);
-
   return (
-    <div className="expedia-widget-wrapper" style={{ width: '100%', minHeight: '350px', background: 'transparent' }}>
-      {/* The Expedia Widget Container */}
-      <div 
-        className="eg-widget" 
-        data-widget="search" 
-        data-program="us-expedia" 
-        data-lobs="stays,flights" 
-        data-network="pz" 
-        data-camref="1110lNcdP" 
-        data-pubref=""
-      ></div>
-      
-      {/* Script Injection Point */}
-      <div ref={scriptContainerRef}></div>
+    <div className="expedia-widget-wrapper" style={{ width: '100%', minHeight: '400px', background: 'transparent' }}>
+      <iframe 
+        src="/expedia.html" 
+        style={{ width: '100%', height: '400px', border: 'none', overflow: 'hidden' }}
+        title="Expedia Search"
+        scrolling="no"
+      ></iframe>
     </div>
   );
 };
